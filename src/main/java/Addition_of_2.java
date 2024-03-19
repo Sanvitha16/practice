a3d3ffba672d29b892da0b7bc2c5c0b4e92141d5
sanvithasamudrala <sanvitha2003@gmail.com>
2024-03-19 14:58:16 +0530

    addition code-first time

--- src/main/java/Addition_of_2.java
+++ src/main/java/Addition_of_2.java
@@ -1,12 +1,12 @@
 package com.qualizeal.javaapp.mathematics;
 
-public class Addition_of_3 {
-	public void add_of_3() {
+public class Addition_of_2 {
+	public void add_of_2() {
 		int num1 = 2;
 		int num2 = 6;
-		int num3 = 4;
+		//int num3 = 4;
 
-		int res = num1 + num2 + num3;
+		int res = num1 + num2;
 
 		System.out.println(res);
 	}
