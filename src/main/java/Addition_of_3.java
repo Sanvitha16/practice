95cb29535c4cbe6b5605277a5bfcdf329cc08c27
sanvithasamudrala <sanvitha2003@gmail.com>
2024-03-19 13:07:00 +0530

    added code - first time

---
+++ src/main/java/Addition_of_3.java
@@ -0,0 +1,13 @@
+package com.qualizeal.javaapp.mathematics;
+
+public class Addition {
+	public void add_of_3() {
+		int num1 = 2;
+		int num2 = 6;
+		int num3 = 4;
+
+		int res = num1 + num2 + num3;
+
+		System.out.println(res);
+	}
+}
